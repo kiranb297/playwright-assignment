b6e8a59534d9930a6ad387577546879bbd7fef07
Kiran <bkiran@297@gmail.com>
2024-07-03 20:52:20 +0530

    Modified test script

--- tests/assignment.spec.ts
+++ tests/assignment.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 // Test case to search for Shoes in Amazon
-test('Search for "Shoes', async ({ page }) => {
+test('Search for "Shoes"', async ({ page }) => {
   // Launching amazon url.
   await page.goto("https://www.amazon.in/");
 
@@ -11,9 +11,9 @@ test('Search for "Shoes', async ({ page }) => {
   // Search for Shoes in the Search bar.
   await page.getByPlaceholder('Search Amazon.in').fill("Shoes");
   await page.keyboard.press('Enter');
-  await expect(page.getByText('Results', { exact: true })).toBeVisible()
+  await expect(page.getByText('Results', { exact: true }), 'Verify Result page').toBeVisible();
 
   // Verify it is showing results for "Shoes".
-  await expect(page.locator("//span[contains(@data-component-type,'result-info')]//span[contains(text(),'results')]/following-sibling::span[@class]"))
-    .toContainText("Shoes");
+  await expect(page.locator("//span[contains(@data-component-type,'result-info')]//span[contains(text(),'results')]/following-sibling::span[@class]"),
+    'Verify showing results for searched keyword "Shoes"').toContainText("Shoes");
 });
